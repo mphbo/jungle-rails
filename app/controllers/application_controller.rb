498ede713a19a3e60365d693ed5b1e7098f25ea1
Logan Thomas <logannormanthomas@protonmail.com>
2021-06-23 06:56:56 +0000

    added user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  http_basic_authenticate_with name: 'Jungle', password: 'book', only: [:admin]
   protect_from_forgery with: :exception
 
   private
