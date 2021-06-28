840cefb8ab990645b606b776ea6ea909f9112ed4
Logan Thomas <logannormanthomas@protonmail.com>
2021-06-28 21:47:39 +0000

    completed testing suites installs

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ActiveRecord::Base
   validates :password, length: {minimum: 3}
   validates :password_confirmation, :presence => true
 
+   def self.authenticate_with_credentials(email, password)
+    user_email = email.strip.downcase
+    @user = User.find_by_email(user_email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe User, type: :model do
   describe "Validations" do
 
     it 'will not pass validation with mismatched password and password_confirmation' do
-      @user = User.new(name: 'Larry', password: 'password', password_confirmation: 'pass', email: 'example@example.com')
+      @user = User.new(first_name: 'Larry', last_name: 'Tate', password: 'password', password_confirmation: 'pass', email: 'example@example.com')
 
       expect(@user).to_not be_valid
     end
 
     it 'Will not pass validation with an email that is already in the database' do
-      @user1 = User.new(name: 'Larry', password: 'password', password_confirmation: 'password', email: 'email@email.com')
+      @user1 = User.new(first_name: 'Larry', last_name: 'Tate', password: 'password', password_confirmation: 'password', email: 'email@email.com')
       @user1.save
       @user2 = User.new(password: 'pass', password_confirmation: 'pass', email: 'email@email.com')
 
@@ -24,13 +24,37 @@ RSpec.describe User, type: :model do
     end
 
     it 'Will not pass validation with a password that is less than 3 characters' do
-      @user2 = User.new(name: 'Larry', password: 'pa', password_confirmation: 'pa', email: 'email2@email.com')
+      @user2 = User.new(first_name: 'Larry', last_name: 'Tate', password: 'pa', password_confirmation: 'pa', email: 'email2@email.com')
 
       expect(@user2).to_not be_valid
     end
 
-    
+  end
 
-    
+  describe '.authenticate_with_credentials' do
+
+    it 'should match password and email' do
+      user = User.create(first_name: 'Larry', last_name: 'Tate', email: 'email@email.com', password: 'password', password_confirmation: 'password')
+
+      session = User.authenticate_with_credentials('email@email.com', 'password')
+      expect(session).to eq user
+    end
+
+   it 'should match when there is a space in front and at the end of email' do
+      user = User.create(first_name: 'Larry', last_name: 'Tate', email: 'email@email.com', password: 'password', password_confirmation: 'password')
+
+      session = User.authenticate_with_credentials(' email@email.com ', 'password')
+      expect(session).to eq user
+    end 
+
+  it 'should match when user uses wrong cases' do
+      user = User.create(first_name: 'Larry', last_name: 'Tate', email: 'email@email.com', password: 'password', password_confirmation: 'password')
+
+      session = User.authenticate_with_credentials('email@Email.com', 'password')
+      expect(session).to eq user
+    end
   end
 end
+
+
+
